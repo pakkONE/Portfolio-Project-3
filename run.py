f990a1b20142711f48e4b3da3d06eca62b440ca6
pakkONE <johan.pakarinen@gmail.com>
2022-05-04 08:33:52 +0000

    Define the first function to welcome the user and describes what the program will do

--- run.py
+++ run.py
@@ -16,4 +16,17 @@ employees = SHEET.worksheet('Employees')
 
 data = employees.get_all_values()
 
-print(data)
+
+def welcome_text():
+    """
+    A welcome message will be printed and explaining which commands
+    are available to extract certain data about employees.
+    """
+    print("Welcome to My Company Data\n")
+    print("This program will give you a chance to extract\n")
+    print("anonymous data about the employees of this company")
+    print("The available commands are (type the number into the console):\n")
+    print("1. Nationalities\n2. Genders\n3. Salary")
+    get_input_data()
+
+
