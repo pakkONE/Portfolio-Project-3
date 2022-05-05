4cc140988851d151dc2649e4af53dfaf5887eb00
pakkONE <johan.pakarinen@gmail.com>
2022-05-05 15:42:59 +0000

    Refactor and clean up code to take up less space

--- run.py
+++ run.py
@@ -10,11 +10,6 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('my-company-data')
-
-employees = SHEET.worksheet('Employees')
-
-data = employees.get_all_values()
 
 
 def welcome_text():
@@ -23,11 +18,18 @@ def welcome_text():
     are available to extract certain data about employees.
     """
     print("Welcome to My Company Data\n")
-    print("This program will give you a chance to extract\n")
-    print("anonymous data about the employees of this company")
+    print("This program will give you a chance to extract")
+    print("anonymous data about the employees of this company\n")
     print("The available commands are (type the number into the console):\n")
     print("1. Nationalities\n2. Genders\n3. Salary")
     get_input_data()
+
+
+def validate_input(values):
+    if values not in ['1', '2', '3']:
+        print("Invalid data, please try again")
+        return False
+    return True
 
 
 def get_input_data():
@@ -36,53 +38,56 @@ def get_input_data():
     dependant on if the value entered is valid.
     """
     command = input("Enter a number listed above to view the data\n")
+
     print("You have selected: " + command)
 
-    while True:
-        if command == 1:
-            nationalities_data(1)
-        elif command == 2:
-            gender_data(2)
-        elif command == 3:
-            salary_data(3)
-        
-        if validate_input(True):
-            print("Your input has been validated, printing data to terminal")
-        break
-    return command
+    if validate_input(command):
+        print("Your input has been validated, printing data to terminal")
+        if command == '1':
+            nationalities_data()
+        elif command == '2':
+            gender_data()
+        elif command == '3':
+            salary_data()
+    else:
+        get_input_data()
 
 
-def validate_input(values):
-    try:
-        if values != int(1 or 2 or 3):
-            raise ValueError(
-                'You have not entered one of the numbers provided above'
-                )
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again")
-        return False
-
-    return True
+def fetch_new_data():
+    sheet = GSPREAD_CLIENT.open('my-company-data')
+    return sheet.worksheet('Employees')
 
 
 def nationalities_data():
-    countries = employees.col_values(5)
     print("Nationalities selected")
+    employees = fetch_new_data()
+    countries = employees.col_values(5)
+    print(countries)
     return countries
 
 
 def gender_data():
-    gender = employees.col_values(4)
     print("Genders selected")
-    return gender
+    employees = fetch_new_data()
+    gender = employees.col_values(4)
     print(gender)
+    return gender
 
 
 def salary_data():
-    salary = employees.col_values(6)
     print("Salaries selected")
-    return salary
+    employees = fetch_new_data()
+    salary = employees.col_values(6)
     print(salary)
+    return salary
 
 
-welcome_text()
+def main():
+    """
+    Run all functions
+    """
+    welcome_text()
+
+
+if __name__ == '__main__':
+    main()
